9ef53df716acd4fe0c13a919f1e703b18bc6dbb0
Jaward Sesay <jkef96@icloud.com>
2023-01-11 19:14:29 +0800

    state management stores - pinia

---
+++ src/stores/ImageStore.js
@@ -0,0 +1,27 @@
+import { defineStore } from "pinia";
+import { Configuration, OpenAIApi } from "openai";
+
+export const useImageStore=  defineStore('ImageStore',{
+    state: () => ({
+        Image:null,
+        name:"Jaykef",
+        data:null
+    }),
+    actions:{
+        async getImage(value){
+            const configuration = new Configuration({
+                apiKey: import.meta.env.VITE_apiKey,
+            });              
+            const openai = new OpenAIApi(configuration);
+              this.image = await openai.createImage({
+                  prompt: value,
+                  n: 6,
+                  size: "256x256",
+              });
+              this.data = this.image.data.data;
+              console.log(this.data)
+        }
+    },
+    getters:{
+    }
+})
