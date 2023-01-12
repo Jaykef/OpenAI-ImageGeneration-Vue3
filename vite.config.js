c74a3ada3cc26abae0a82e64bad46f72199146ae
Jaward Sesay <jkef96@icloud.com>
2023-01-12 20:55:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/ImageGeneration-OpenAI-VueJs/",
+  base:"/OpenAI-ImageGeneration-Vue3/",
   plugins: [vue()],
   resolve: {
     alias: {
